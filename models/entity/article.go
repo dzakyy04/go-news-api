a75fef44a8effc3bdebb3960a4581fccb95ee735
dzakyy04 <dzakylinggau@gmail.com>
2024-07-02 22:29:09 +0700

    feat: add migration for articles table

---
+++ models/entity/article.go
@@ -0,0 +1,26 @@
+package entity
+
+import "time"
+
+type ArticleStatus string
+
+const (
+	Published ArticleStatus = "published"
+	Draft     ArticleStatus = "draft"
+	Review    ArticleStatus = "review"
+)
+
+type Article struct {
+	ID         uint          `gorm:"primaryKey" json:"id"`
+	Title      string        `gorm:"type:varchar(100);not null" json:"title"`
+	Slug       string        `gorm:"type:varchar(100);not null;unique" json:"slug"`
+	Thumbnail  string        `gorm:"type:varchar(100);not null" json:"thumbnail"`
+	Content    string        `gorm:"type:text;not null" json:"content"`
+	CategoryID uint          `gorm:"not null" json:"category_id"`
+	Category   Category      `gorm:"foreignKey:CategoryID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE" json:"category"`
+	Author     User          `gorm:"foreignKey:AuthorID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE" json:"author"`
+	AuthorID   uint          `gorm:"not null" json:"author_id"`
+	Status     ArticleStatus `gorm:"type:enum('draft', 'published', 'archived');default:draft" json:"status"`
+	CreatedAt  time.Time     `json:"created_at"`
+	UpdatedAt  time.Time     `json:"updated_at"`
+}
