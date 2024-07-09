0b994136947f00baa389c80945535907a3eba7ce
dzakyy04 <dzakylinggau@gmail.com>
2024-07-09 23:43:12 +0700

    feat: add migration for comments table

--- database/migration.go
+++ database/migration.go
@@ -6,7 +6,7 @@ import (
 )
 
 func MigrateDatabase() {
-	err := DB.AutoMigrate(&entity.Category{}, &entity.User{}, &entity.OtpCode{}, &entity.Article{})
+	err := DB.AutoMigrate(&entity.Category{}, &entity.User{}, &entity.OtpCode{}, &entity.Article{}, &entity.Comment{})
 	if err != nil {
 		panic("Failed to migrate database: " + err.Error())
 	}
---
+++ models/entity/comment.go
@@ -0,0 +1,16 @@
+package entity
+
+import (
+	"time"
+)
+
+type Comment struct {
+	ID        uint      `gorm:"primaryKey" json:"id"`
+	Content   string    `gorm:"type:text;not null" json:"content"`
+	UserID    uint      `gorm:"not null" json:"user_id"`
+	User      User      `gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE" json:"user"`
+	ArticleID uint      `gorm:"not null" json:"article_id"`
+	Article   Article   `gorm:"foreignKey:ArticleID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE" json:"article"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
